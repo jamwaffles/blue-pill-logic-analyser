360fa3df07527f7d8f83c048ea0dadb0811abac9
James Waples <jamwaffles@gmail.com>
2018-09-01 18:17:47 +0100

    Spam The string "1001" to serial

--- src/main.rs
+++ src/main.rs
@@ -10,152 +10,14 @@ extern crate stm32f103xx_hal as hal;
 extern crate stm32f103xx_usb;
 extern crate usb_device;
 
+mod cdc;
+
 use hal::prelude::*;
 use hal::stm32f103xx;
 use rt::ExceptionFrame;
 
 use stm32f103xx_usb::UsbBus;
 use usb_device::prelude::*;
-
-// Minimal CDC-ACM implementation
-mod cdc_acm {
-    use core::cell::RefCell;
-    use core::cmp::min;
-    use usb_device::class_prelude::*;
-    use usb_device::Result;
-
-    pub const USB_CLASS_CDC: u8 = 0x02;
-    const USB_CLASS_DATA: u8 = 0x0a;
-    const CDC_SUBCLASS_ACM: u8 = 0x02;
-    const CDC_PROTOCOL_AT: u8 = 0x01;
-
-    const CS_INTERFACE: u8 = 0x24;
-    const CDC_TYPE_HEADER: u8 = 0x00;
-    const CDC_TYPE_CALL_MANAGEMENT: u8 = 0x01;
-    const CDC_TYPE_ACM: u8 = 0x02;
-    const CDC_TYPE_UNION: u8 = 0x06;
-
-    const REQ_SET_LINE_CODING: u8 = 0x20;
-    const REQ_SET_CONTROL_LINE_STATE: u8 = 0x22;
-
-    struct Buf {
-        buf: [u8; 64],
-        len: usize,
-    }
-
-    pub struct SerialPort<'a, B: 'a + UsbBus> {
-        comm_if: InterfaceNumber,
-        comm_ep: EndpointIn<'a, B>,
-        data_if: InterfaceNumber,
-        read_ep: EndpointOut<'a, B>,
-        write_ep: EndpointIn<'a, B>,
-
-        read_buf: RefCell<Buf>,
-    }
-
-    impl<'a, B: UsbBus> SerialPort<'a, B> {
-        pub fn new(alloc: &UsbAllocator<'a, B>) -> SerialPort<'a, B> {
-            SerialPort {
-                comm_if: alloc.interface(),
-                comm_ep: alloc.interrupt(8, 255),
-                data_if: alloc.interface(),
-                read_ep: alloc.bulk(64),
-                write_ep: alloc.bulk(64),
-                read_buf: RefCell::new(Buf {
-                    buf: [0; 64],
-                    len: 0,
-                }),
-            }
-        }
-
-        pub fn write(&self, data: &[u8]) -> Result<usize> {
-            match self.write_ep.write(data) {
-                Ok(count) => Ok(count),
-                Err(UsbError::Busy) => Ok(0),
-                e => e,
-            }
-        }
-
-        pub fn read(&self, data: &mut [u8]) -> Result<usize> {
-            let mut buf = self.read_buf.borrow_mut();
-
-            // Terrible buffering implementation for brevity's sake
-
-            if buf.len == 0 {
-                buf.len = match self.read_ep.read(&mut buf.buf) {
-                    Ok(count) => count,
-                    Err(UsbError::NoData) => return Ok(0),
-                    e => return e,
-                };
-            }
-
-            if buf.len == 0 {
-                return Ok(0);
-            }
-
-            let count = min(data.len(), buf.len);
-
-            &data[..count].copy_from_slice(&buf.buf[0..count]);
-
-            buf.buf.rotate_left(count);
-            buf.len -= count;
-
-            Ok(count)
-        }
-    }
-
-    impl<'a, B: UsbBus> UsbClass for SerialPort<'a, B> {
-        fn get_configuration_descriptors(&self, writer: &mut DescriptorWriter) -> Result<()> {
-            // TODO: make a better DescriptorWriter to make it harder to make invalid descriptors
-            writer.interface(
-                self.comm_if,
-                1,
-                USB_CLASS_CDC,
-                CDC_SUBCLASS_ACM,
-                CDC_PROTOCOL_AT,
-            )?;
-
-            writer.write(CS_INTERFACE, &[CDC_TYPE_HEADER, 0x10, 0x01])?;
-
-            writer.write(
-                CS_INTERFACE,
-                &[CDC_TYPE_CALL_MANAGEMENT, 0x00, self.data_if.into()],
-            )?;
-
-            writer.write(CS_INTERFACE, &[CDC_TYPE_ACM, 0x00])?;
-
-            writer.write(
-                CS_INTERFACE,
-                &[CDC_TYPE_UNION, self.comm_if.into(), self.data_if.into()],
-            )?;
-
-            writer.endpoint(&self.comm_ep)?;
-
-            writer.interface(self.data_if, 2, USB_CLASS_DATA, 0x00, 0x00)?;
-
-            writer.endpoint(&self.write_ep)?;
-            writer.endpoint(&self.read_ep)?;
-
-            Ok(())
-        }
-
-        fn control_out(&self, req: &control::Request, buf: &[u8]) -> ControlOutResult {
-            let _ = buf;
-
-            if req.request_type == control::RequestType::Class
-                && req.recipient == control::Recipient::Interface
-            {
-                return match req.request {
-                    REQ_SET_LINE_CODING => ControlOutResult::Ok,
-                    REQ_SET_CONTROL_LINE_STATE => ControlOutResult::Ok,
-                    _ => ControlOutResult::Ignore,
-                };
-            }
-
-            ControlOutResult::Ignore
-        }
-    }
-}
 
 entry!(main);
 fn main() -> ! {
@@ -164,8 +26,7 @@ fn main() -> ! {
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
 
-    let clocks = rcc
-        .cfgr
+    let clocks = rcc.cfgr
         .hse(8.mhz())
         .sysclk(48.mhz())
         .pclk1(24.mhz())
@@ -180,34 +41,36 @@ fn main() -> ! {
         .resetter(&clocks, &mut gpioa.crh, gpioa.pa12)
         .reset();
 
-    let serial = cdc_acm::SerialPort::new(&usb_bus.allocator());
+    let serial = cdc::SerialPort::new(&usb_bus.allocator());
 
     let usb_dev = UsbDevice::new(&usb_bus, UsbVidPid(0xeeee, 0xffff))
         .manufacturer("Fake company")
         .product("Serial port")
         .serial_number("TEST")
-        .device_class(cdc_acm::USB_CLASS_CDC)
+        .device_class(cdc::USB_CLASS_CDC)
         .build(&[&serial]);
 
     loop {
         usb_dev.poll();
 
         if usb_dev.state() == UsbDeviceState::Configured {
-            let mut buf = [0u8; 8];
+            // let mut buf = [0u8; 8];
 
-            match serial.read(&mut buf) {
-                Ok(count) if count > 0 => {
-                    // Echo back in upper case
-                    for c in buf[0..count].iter_mut() {
-                        if 0x61 <= *c && *c <= 0x7a {
-                            *c &= !0x20;
-                        }
-                    }
+            // match serial.read(&mut buf) {
+            //     Ok(count) if count > 0 => {
+            //         // Echo back in upper case
+            //         for c in buf[0..count].iter_mut() {
+            //             if 0x61 <= *c && *c <= 0x7a {
+            //                 *c &= !0x20;
+            //             }
+            //         }
 
-                    serial.write(&buf[0..count]).ok();
-                }
-                _ => {}
-            }
+            //         serial.write(&buf[0..count]).ok();
+            //     }
+            //     _ => {}
+            // }
+
+            serial.write(b"1001\n").ok();
         }
     }
 }
