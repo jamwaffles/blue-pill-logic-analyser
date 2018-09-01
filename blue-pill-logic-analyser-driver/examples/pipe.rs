b86ec7ce8987e11ca75c0f7e8afb879fe5cda04c
James Waples <jamwaffles@gmail.com>
2018-09-01 22:05:54 +0100

    Error handling

--- blue-pill-logic-analyser-driver/examples/pipe.rs
+++ blue-pill-logic-analyser-driver/examples/pipe.rs
@@ -5,27 +5,30 @@ extern crate serial;
 
 use blue_pill_logic_analyser_driver::configure;
 use std::env;
+use std::error::Error;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::time::Duration;
+
+struct DataPoint {
+    value: u8,
+    time: u32,
+}
 
 fn main() {
+    let max_time = Duration::from_millis(10000);
+
     for arg in env::args_os().skip(1) {
         let mut port = serial::open(&arg).unwrap();
         configure(&mut port).unwrap();
 
         let mut file = BufReader::new(port);
+
         for line in file.lines() {
-            let l = line.unwrap();
-
-            println!("{}", l);
+            match line {
+                Ok(l) => println!("{:?}", l),
+                Err(e) => eprintln!("PARSE ERR {}", e.description()),
+            }
         }
-
-        // let mut buf: [u8; 1024] = [0; 1024];
-
-        // loop {
-        //     port.read(&mut buf).ok();
-
-        //     println!("{}NEXT", String::from_utf8(buf.to_vec()).unwrap());
-        // }
     }
 }
