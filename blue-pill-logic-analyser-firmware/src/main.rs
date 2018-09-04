017cb7eb0ef7e77bf398699d1ec25167d91729c8
James Waples <jamwaffles@gmail.com>
2018-09-04 16:47:40 +0100

    Bump deps, use new way of doing interrupts

--- blue-pill-logic-analyser-firmware/src/main.rs
+++ blue-pill-logic-analyser-firmware/src/main.rs
@@ -7,133 +7,175 @@ extern crate cortex_m_rt as rt;
 extern crate cortex_m_rtfm as rtfm;
 extern crate cortex_m_rtfm_macros;
 extern crate panic_semihosting;
+#[macro_use]
+extern crate stm32f103xx;
 extern crate stm32f103xx_hal as hal;
 extern crate stm32f103xx_usb;
 extern crate usb_device;
 
 mod cdc;
 
-use cortex_m_rtfm_macros::app;
-use hal::prelude::*;
-// use hal::stm32f103xx;
 use cdc::SerialPort;
+use cortex_m::peripheral::syst::SystClkSource;
 use hal::gpio::gpiob::PB9;
-use hal::gpio::{Alternate, Floating, Input, Output, PushPull};
+use hal::gpio::{Floating, Input};
+use hal::prelude::*;
+use hal::stm32f103xx::Interrupt;
 use rt::ExceptionFrame;
-use rtfm::Threshold;
 use stm32f103xx_usb::UsbBus;
 use usb_device::prelude::*;
 
-// Tasks and resources
-app! {
-    device: hal::stm32f103xx,
+// entry!(main);
 
-    resources: {
-        // static USB: UsbDevice<'static, UsbBus>;
-        static SERIAL: SerialPort<'static, UsbBus>;
-        static INPUT_PIN: PB9<Input<Floating>>;
-    },
+// fn init(p: init::Peripherals) -> init::LateResources {
+//     let mut flash = p.device.FLASH.constrain();
+//     let mut rcc = p.device.RCC.constrain();
 
-    idle: {
-        resources: [
-            INPUT_PIN
-        ],
-    },
+//     let clocks = rcc
+//         .cfgr
+//         .hse(8.mhz())
+//         .sysclk(48.mhz())
+//         .pclk1(24.mhz())
+//         .freeze(&mut flash.acr);
 
-    tasks: {
-        SYS_TICK: {
-            path: tick,
-            resources: [
-                SERIAL
-            ],
-        },
-    },
-}
+//     assert!(clocks.usbclk_valid());
 
-fn ent() -> ! {
-    main();
+//     let usb_bus = UsbBus::usb(p.device.USB, &mut rcc.apb1);
 
-    loop {}
-}
+//     let mut gpioa = p.device.GPIOA.split(&mut rcc.apb2);
+//     let mut gpiob = p.device.GPIOB.split(&mut rcc.apb2);
+//     usb_bus
+//         .resetter(&clocks, &mut gpioa.crh, gpioa.pa12)
+//         .reset();
 
-entry!(ent);
+//     let serial = SerialPort::new(&usb_bus.allocator());
 
-fn init(p: init::Peripherals) -> init::LateResources {
-    let mut flash = p.device.FLASH.constrain();
-    let mut rcc = p.device.RCC.constrain();
+//     let usb_dev = UsbDevice::new(&usb_bus, UsbVidPid(0xeeee, 0xffff))
+//         .manufacturer("Fake company")
+//         .product("Serial port")
+//         .serial_number("TEST")
+//         .device_class(cdc::USB_CLASS_CDC)
+//         .build(&[&serial]);
 
-    let clocks = rcc.cfgr
-        .hse(8.mhz())
-        .sysclk(48.mhz())
-        .pclk1(24.mhz())
-        .freeze(&mut flash.acr);
+//     let input_pin = gpiob.pb9.into_floating_input(&mut gpiob.crh);
 
-    assert!(clocks.usbclk_valid());
+//     init::LateResources {
+//         // USB: usb_dev,
+//         SERIAL: serial,
+//         INPUT_PIN: input_pin,
+//     }
+// }
 
-    let usb_bus = UsbBus::usb(p.device.USB, &mut rcc.apb1);
+// fn idle(_t: &mut Threshold, r: idle::Resources) -> ! {
+// let usb_dev: &'static mut UsbDevice = r.USB;
+// let serial: &'static mut SerialPort = r.SERIAL;
+// let input_pin: &'static mut u8 = r.INPUT_PIN;
 
-    let mut gpioa = p.device.GPIOA.split(&mut rcc.apb2);
-    let mut gpiob = p.device.GPIOB.split(&mut rcc.apb2);
-    usb_bus
-        .resetter(&clocks, &mut gpioa.crh, gpioa.pa12)
-        .reset();
+// loop {
+//     usb_dev.poll();
 
-    let serial = SerialPort::new(&usb_bus.allocator());
+//     if usb_dev.state() == UsbDeviceState::Configured {
+//         // let mut buf = [0u8; 8];
 
-    let usb_dev = UsbDevice::new(&usb_bus, UsbVidPid(0xeeee, 0xffff))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
-        .device_class(cdc::USB_CLASS_CDC)
-        .build(&[&serial]);
+//         // match serial.read(&mut buf) {
+//         //     Ok(count) if count > 0 => {
+//         //         // Echo back in upper case
+//         //         for c in buf[0..count].iter_mut() {
+//         //             if 0x61 <= *c && *c <= 0x7a {
+//         //                 *c &= !0x20;
+//         //             }
+//         //         }
 
-    let input_pin = gpiob.pb9.into_floating_input(&mut gpiob.crh);
+//         //         serial.write(&buf[0..count]).ok();
+//         //     }
+//         //     _ => {}
+//         // }
+//         if input_pin.is_high() {
+//             serial.write(b"255\n").ok();
+//         } else {
+//             serial.write(b"0\n").ok();
+//         }
+//     }
 
-    init::LateResources {
-        // USB: usb_dev,
-        SERIAL: serial,
-        INPUT_PIN: input_pin,
+//     rtfm::wfi();
+// }
+
+//     loop {}
+// }
+
+entry!(main);
+
+fn main() -> ! {
+    let p = cortex_m::Peripherals::take().unwrap();
+
+    let mut syst = p.SYST;
+    let mut nvic = p.NVIC;
+
+    nvic.enable(Interrupt::EXTI0);
+
+    // configure the system timer to wrap around every second
+    syst.set_clock_source(SystClkSource::Core);
+    syst.set_reload(8_000_000); // 1s
+    syst.enable_counter();
+
+    loop {
+        // busy wait until the timer wraps around
+        while !syst.has_wrapped() {}
+
+        // trigger the `EXTI0` interrupt
+        nvic.set_pending(Interrupt::EXTI0);
     }
 }
 
-fn idle(_t: &mut Threshold, r: idle::Resources) -> ! {
-    // let usb_dev: &'static mut UsbDevice = r.USB;
-    // let serial: &'static mut SerialPort = r.SERIAL;
-    // let input_pin: &'static mut u8 = r.INPUT_PIN;
+exception!(
+    SysTick,
+    sys_tick,
+    state: Option<(UsbDevice<UsbBus>, SerialPort<UsbBus>, PB9<Input<Floating>>)> = None
+);
 
-    // loop {
-    //     usb_dev.poll();
+fn sys_tick(state: &mut Option<(UsbDevice<UsbBus>, SerialPort<UsbBus>, PB9<Input<Floating>>)>) {
+    if state.is_none() {
+        // let cp = cortex_m::Peripherals::take().unwrap();
+        let dp = stm32f103xx::Peripherals::take().unwrap();
 
-    //     if usb_dev.state() == UsbDeviceState::Configured {
-    //         // let mut buf = [0u8; 8];
+        let mut flash = dp.FLASH.constrain();
+        let mut rcc = dp.RCC.constrain();
 
-    //         // match serial.read(&mut buf) {
-    //         //     Ok(count) if count > 0 => {
-    //         //         // Echo back in upper case
-    //         //         for c in buf[0..count].iter_mut() {
-    //         //             if 0x61 <= *c && *c <= 0x7a {
-    //         //                 *c &= !0x20;
-    //         //             }
-    //         //         }
+        let clocks = rcc
+            .cfgr
+            .hse(8.mhz())
+            .sysclk(48.mhz())
+            .pclk1(24.mhz())
+            .freeze(&mut flash.acr);
 
-    //         //         serial.write(&buf[0..count]).ok();
-    //         //     }
-    //         //     _ => {}
-    //         // }
-    //         if input_pin.is_high() {
-    //             serial.write(b"255\n").ok();
-    //         } else {
-    //             serial.write(b"0\n").ok();
-    //         }
-    //     }
+        assert!(clocks.usbclk_valid());
 
-    //     rtfm::wfi();
-    // }
+        let usb_bus = UsbBus::usb(dp.USB, &mut rcc.apb1);
 
-    loop {}
+        let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+        let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+        usb_bus
+            .resetter(&clocks, &mut gpioa.crh, gpioa.pa12)
+            .reset();
+
+        let serial = SerialPort::new(&usb_bus.allocator());
+
+        let usb_dev = UsbDevice::new(&usb_bus, UsbVidPid(0xeeee, 0xffff))
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")
+            .device_class(cdc::USB_CLASS_CDC)
+            .build(&[&serial]);
+
+        let input_pin = gpiob.pb9.into_floating_input(&mut gpiob.crh);
+
+        *state = Some((usb_dev, serial, input_pin))
+    }
+
+    if let Some((a, b, c)) = state.as_mut() {
+        // hstdout.write_str(".").unwrap();
+    }
 }
-
-fn tick(_t: &mut Threshold, mut r: SYS_TICK::Resources) {}
 
 exception!(HardFault, hard_fault);
 fn hard_fault(ef: &ExceptionFrame) -> ! {
